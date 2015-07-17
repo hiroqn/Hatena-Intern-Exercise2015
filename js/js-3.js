b3666f5c56ba17697f53e1faf9e49a8f6d0209f2
hiroqn <hiroqn1008@gmail.com>
2015-07-17 20:31:20 +0900

    🎧 Now's The Time / Art Blakey #nowplaying

--- js/js-3.js
+++ js/js-3.js
@@ -1,1 +1,6 @@
 // 課題 JS-3 の実装をここに記述してください。
+document.addEventListener('DOMContentLoaded', function (e) {
+    document.getElementById('submit-button').addEventListener('click', function (e) {
+      createLogTable(document.getElementById('table-container'), parseLTSVLog(document.getElementById('log-input').value));
+    })
+});
--- js/main.js
+++ js/main.js
@@ -1,3 +1,39 @@
 // 課題 JS-1: 関数 `parseLTSVLog` を記述してください
 
 // 課題 JS-2: 関数 `createLogTable` を記述してください
+
+function parseLTSVLog (logStr) {
+  function typeConverter (key, value) {
+    switch (key) {
+      case 'epoch':
+        return value - 0;
+        break;
+      default:
+        return value;
+        break;
+    }
+  }
+  return logStr.split('\n').filter(function (lineStr) {
+    return lineStr.length > 0;
+  }).map(function (lineStr) {
+    return lineStr.split('\t').reduce(function (prev, elemStr) {
+      var temp = elemStr.split(':');
+      prev[temp[0]] = typeConverter(temp[0], temp[1]);
+      return prev;
+    }, {});
+  });
+}
+
+function createLogTable (elem, logObj) {
+  elem.innerHTML = [
+    '<table>',
+    '<thead><tr><th>path</th><th>epoch</th></tr></thead>',
+    '<tbody>',
+    logObj.map(function (obj) {
+      return ['<tr><td>', obj.path, '</td><td>',obj.epoch ,'</td></tr>'].join('')
+    }).join(''),
+    '</tbody>',
+    '</table>'
+  ].join('');
+  return elem;
+}
