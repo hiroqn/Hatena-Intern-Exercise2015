62914e886a0202c84ccb8c588126c6e4a20661e0
hiroqn <hiroqn1008@gmail.com>
2015-08-01 22:06:06 +0900

    必殺！！新幹線コーディングだぁぁぁ！！

--- perl/main4.pl
+++ perl/main4.pl
@@ -1,6 +1,6 @@
 use strict;
 use warnings;
-
+use Term::Screen;
 use Data::Dumper;
 
 use Parser;
@@ -10,14 +10,45 @@ my $parser = Parser->new( filename => '../sample_data/large_log.ltsv' );
 
 my $counter = LogCounter->new($parser->parse);
 my $path =  $counter->group_by_path;
+my @path_list = sort(keys($path));
+my $scr = Term::Screen->new();
 
-for my $p (sort keys($path)) {
-  print "$p\n";
-  print "---:\n";
-  my $status =  LogCounter->new($path->{$p})->group_by_status;
-  for my $stat (sort keys($status)) {
-    my $count = @{$status->{$stat}};
-    print "$stat:" . ("=" x $count) . "\n";
+$scr->clrscr()->noecho();
+
+my $path_list_length = $#path_list + 1;
+$scr->at(0,0)->puts("Choose path . press q and exit");
+sub render {
+  my ($pos) = @_;
+  my $count = 0;
+  for my $p (@path_list) {
+    if ($pos == $count) {
+      $scr->at($count + 1, 0)->reverse()->puts($p)->normal();
+    } else {
+      $scr->at($count + 1, 0)->puts($p);
+    }
+    $count++;
   }
-  print "\n";
+  # print Dumper
+  my $status =  LogCounter->new($path->{$path_list[$pos]})->group_by_status;
+  $scr->clreos();
+  $scr->at($count + 1, 0)->puts("---:     5    10        20");
+  for my $stat (sort keys($status)) {
+    my $length = @{$status->{$stat}};
+    $scr->at($count + 2, 0)->puts("$stat:" . ("=" x $length));
+    $count++;
+  }
+  $scr->at($count + 2, 0);
+}
+
+my $ch = '';
+render(0);
+my $index = 0;
+
+while(($ch = $scr->getch()) ne 'q') {
+  if ($ch eq "ku") {
+    $index = ($index - 1) % $path_list_length;
+  } elsif ($ch eq "kd") {
+    $index = ($index + 1) % $path_list_length;
+  }
+  render($index);
 }
